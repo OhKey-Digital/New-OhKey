3012a7bec84447b2c2935d93b9f707c14a3945c1
Astaroth25 <astaroth25.dev@gmail.com>
2026-05-19 16:01:24 -0500

    fix: solución de error al desplegar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'hybrid',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  })
+  output: 'hybrid', 
+  adapter: vercel(),
 });
